562b8852e0dbf7020807cfb2a779df4a2119c3bc
fbily <flamby50@gmail.com>
2023-10-03 17:30:28 +0200

    Wordpress quasi ok.

--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -11,7 +11,9 @@ mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root
 wp config create --dbhost=$BDD_HOST --dbname=$BDD_NAME --dbuser=$BDD_USER --dbpass=$BDD_USER_PASSWORD --allow-root
-wp core install --url=fbily.42.fr --title="Inception" --admin_name=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL --allow-root
-wp user create $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PASSWORD --allow-root
+wp core install --url=fbily.42.fr --title="Inception - fbily" --admin_name=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL --skip-email --allow-root
+wp user create $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
+
+wp theme install inspiro --activate --allow-root
 
 /usr/sbin/php-fpm7.4 -F
